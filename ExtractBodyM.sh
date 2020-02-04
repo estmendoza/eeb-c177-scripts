467f05e595e5bd01724d9e92294a36ecc95bc1ae
Esteban Mendoza <emendoza15331@gmail.com>
2020-02-04 09:06:48 -0800

    dynamic script

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,7 +1,10 @@
 #!/bin/bash 
 #written script will be for the Bash shell
 
-tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr -s ";" " " | sort -r -n -k 6 
+read -p "What is the file name? " file
+read -p "What is the delimiter? " deli
+read -p "What do you want to rename the file? " ren
+tail -n +2 ~/Developer/repos/CSB/unix/data/$file | cut -d "$deli" -f 2-6 | tr -s ";" " " | sort -r -n -k 6 > $ren.csv
 # ignore the first row in the file | the deliminator is ; so take rows 2-6 | change the deliminator from ; to a space | sort the rows, focusing on numeric value column 6, in reverse 
 
 #To make the script run from any prompt without typing bash, you need to add the shell script's directory to your PATH
